e16ed10a57f07ea33eba00486d11d6ec694888ad
France199 <quiachonfrancis@gmail.com>
2024-08-15 10:09:51 +0800

    my first repo

---
+++ src/quaichon2004/Quaichon2004.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quaichon2004;
+
+/**
+ *
+ * @author SCC-COLLEGE
+ */
+public class Quaichon2004 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
